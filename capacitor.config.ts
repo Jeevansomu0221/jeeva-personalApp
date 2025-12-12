5af875da6917cb30c6145ea80560d0ea14244f5c
jeevansomu0221 <jeevansomu.ch@gmail.com>
2025-12-12 18:46:03 +0530

    timetable

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'jeeva-app',
-  webDir: 'dist',  // Change this from 'build' to 'dist'
-  server: {
-    androidScheme: 'https'
-  }
+  webDir: 'dist'
 };
 
 export default config;
